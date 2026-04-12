5ecb57b2130cb8d95f0500090b7f5c921a6b2151
Chucky2401 <chuckydu24@gmail.com>
2026-04-12 21:08:44 +0200

    feat(sh): adapt start.sh for wireguard instead of openvpn

--- src/entrypoint/start.sh
+++ src/entrypoint/start.sh
@@ -5,9 +5,9 @@ CURRENTUID=$(id -u deluge)
 CURRENTGID=$(id -g deluge)
 USERID=$CURRENTUID
 GROUPID=$CURRENTGID
-VPN_FILE=${VPN_FILE:-/.openvpn/vpn.ovpn}
+LOCAL_VPN_FILE="/.wg/vpn.conf"
 
-DIR="/deluge-conf /downloads /.openvpn /entrypoint /template"
+DIR="/deluge-conf /downloads /.wg /entrypoint /template"
 
 echo "************************************"
 echo "*                                  *"
@@ -22,40 +22,51 @@ echo "************************************"
 echo ""
 
 # If no VPN credentials, either by env var or secrets, stop
-if [[ -z "$VPN_USER" || -z "$VPN_PASSWORD" ]] && [[ ! -e /run/secrets/VPN_CREDENTIALS ]]; then
+if [[ -z "$PRIVATE_KEY" || -z "$PRESHARED_KEY" || -z "$ADDRESS" ]] && [[ ! -e /run/secrets/VPN_CREDENTIALS ]]; then
   cat <<EOF
 ==> Please provide a valid VPN username and password
 You can use environment variables:
-  - VPN_USER
-  - VPN_PASSWORD
+  - PRIVATE_KEY
+  - PRESHARED_KEY
+  - ADDRESS
 
-Or a secret name 'VPN_CREDENTIALS'
+Or a secret name 'VPN_CREDENTIALS', the file must have the following format:
+PRIVATE_KEY
+PRESHARED_KEY
+ADDRESS
 EOF
 
   exit 1
 fi
 
 # Define VPN credentials
-VPN_USER=${VPN_USER}
-VPN_PASSWORD=${VPN_PASSWORD}
+LOCAL_PRIVATE_KEY=${PRIVATE_KEY}
+LOCAL_PRESHARED_KEY=${PRESHARED_KEY}
+LOCAL_ADDRESS=${ADDRESS}
 
-if [[ -e /run/secrets/VPN_CREDENTIALS ]]; then
-  { IFS= read -r VPN_USER && IFS= read -r VPN_PASSWORD; } </run/secrets/VPN_CREDENTIALS
+if [[ -e "$VPN_CREDENTIALS" ]]; then
+  { IFS= read -r LOCAL_PRIVATE_KEY && IFS= read -r LOCAL_PRESHARED_KEY && IFS= read -r LOCAL_ADDRESS; } <"$VPN_CREDENTIALS"
 fi
 
-# Define VPN .ovpn file
-if [[ -e /run/secrets/VPN_FILE ]]; then
-  VPN_FILE="/run/secrets/VPN_FILE"
-fi
-
-# If VPN .ovpn file does not exist stop
+# If VPN .conf file does not exist stop
 if [[ ! -e "$VPN_FILE" ]]; then
   cat <<EOF
-You must provide a valid .ovpn file or be sure the file exist here:
+You must provide a valid .conf file or be sure the file exist here:
   ${VPN_FILE}
 EOF
   exit 1
 fi
+
+# Copy secrets file to internal file only if necessary
+if [[ "$VPN_FILE" != "$LOCAL_VPN_FILE" ]]; then
+  cp $VPN_FILE $LOCAL_VPN_FILE
+fi
+
+# Define VPN conf file with data
+sed -i -e "s/\(PrivateKey = \).*/\1${LOCAL_PRIVATE_KEY}/g" $LOCAL_VPN_FILE
+sed -i -e "s/\(PresharedKey = \).*/\1${LOCAL_PRESHARED_KEY}/g" $LOCAL_VPN_FILE
+sed -i -e "s/\(Address = \).*/\1${LOCAL_ADDRESS}/g" $LOCAL_VPN_FILE
+sed -i -e "s/\(DNS = \).*/\1/g" $LOCAL_VPN_FILE
 
 # Deluge web ui credentials
 RANDOM_PASSWORD=$(
@@ -75,15 +86,15 @@ if [[ -z "$DELUGE_DAEMON_USER_LEVEL" ]]; then
   echo "DELUGE_DAEMON_USER_LEVEL is not set. Defaul to administrator (10)"
 fi
 
-DELUGE_DAEMON_USERNAME=${DELUGE_DAEMON_USERNAME:-deluge}
-DELUGE_DAEMON_PASSWORD=${DELUGE_DAEMON_PASSWORD:-$RANDOM_PASSWORD}
-DELUGE_DAEMON_USER_LEVEL=${DELUGE_DAEMON_USER_LEVEL:-10}
+LOCAL_DELUGE_DAEMON_USERNAME=${DELUGE_DAEMON_USERNAME:-deluge}
+LOCAL_DELUGE_DAEMON_PASSWORD=${DELUGE_DAEMON_PASSWORD:-$RANDOM_PASSWORD}
+LOCAL_DELUGE_DAEMON_USER_LEVEL=${DELUGE_DAEMON_USER_LEVEL:-10}
 
 if [[ -e "$DELUGE_DAEMON_PASSWORD" ]]; then
-  DELUGE_DAEMON_PASSWORD=$(cat "$DELUGE_DAEMON_PASSWORD")
+  LOCAL_DELUGE_DAEMON_PASSWORD=$(cat "$DELUGE_DAEMON_PASSWORD")
 fi
 
-DELUGE_CREDENTIALS="${DELUGE_DAEMON_USERNAME}:${DELUGE_DAEMON_PASSWORD}:${DELUGE_DAEMON_USER_LEVEL}"
+DELUGE_CREDENTIALS="${LOCAL_DELUGE_DAEMON_USERNAME}:${LOCAL_DELUGE_DAEMON_PASSWORD}:${LOCAL_DELUGE_DAEMON_USER_LEVEL}"
 
 if ! grep -q "$DELUGE_CREDENTIALS" /deluge-conf/auth; then
   echo "$DELUGE_CREDENTIALS" >/deluge-conf/auth
@@ -112,22 +123,20 @@ if [[ -n "$TZ" ]]; then
   ln -fs /usr/share/zoneinfo/${TZ} /etc/localtime
 fi
 
-# Define VPN credentials files
-cat <<-EOF >/var/lib/deluge/vpn
-${VPN_USER}
-${VPN_PASSWORD}
-EOF
-chmod 400 /var/lib/deluge/vpn
-
 # Clear var
-unset VPN_USER
-unset VPN_PASSWORD
+unset PRIVATE_KEY
+unset PRESHARED_KEY
+unset ADDRESS
+unset LOCAL_PRIVATE_KEY
+unset LOCAL_PRESHARED_KEY
+unset LOCAL_ADDRESS
 
 # Export necessary variable
-export VPN_FILE
+export LOCAL_VPN_FILE
 
 # Empty line to lighten log
 echo ""
 
 # Prepare and run Deluge
+# tail -f /dev/null
 exec python3 ./start.py
